92f7b9dd0c473fda44d8cc5574e5006541acdd2c
Prajil <prajilprabhan924@gmail.com>
2026-07-21 10:45:59 +0530

    feat : Project setup and core icon components added

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Education />
+        <Projects />
+        <Contact />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
